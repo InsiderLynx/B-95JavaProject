64cf4742537dab509b72f75a40e46c73416cba0d
InsiderLynx <InsiderLynx@DESKTOP-H4PAF7P>
2023-06-06 08:49:25 +0630

    Second commit

---
+++ src/pac1/SecondJava.java
@@ -0,0 +1,12 @@
+package pac1;
+
+import javax.swing.JOptionPane;
+
+public class SecondJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Hello World");
+	}
+
+}
